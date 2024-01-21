58144a0dac163ea3ff036bbb39b893f7283ce8d5
Rico Kölling <rico.koelling@uni-jena.de>
2024-01-21 20:59:46 +0100

    changed 3

--- VL03-20240121/aufgabe.cpp
+++ VL03-20240121/aufgabe.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <bitset>
 #include "betriebsmodi.h"
 
 using namespace std;
@@ -15,5 +16,36 @@ int main(int argc, char* argv[])
   }
   text.open (argv[1]);
   k_text.open(argv[2]);
-  ECB(128, text, k_text);
+  ECB(32, text, k_text);
+
+  /*vector<bool> k;
+  vector<char> block;
+  block.push_back('A');
+
+  bitset<8> one_char;
+    for(const char &c: block){
+      one_char = c;
+      cout << one_char << endl;
+      for(int i = one_char.size() - 1; i >= 0; i--){
+        k.push_back(one_char[i]);
+      }
+    }
+
+
+    for (const auto &i : k){
+      cout << i;
+    }
+    cout << endl;
+
+    bitset<8> out;
+    size_t count = out.size() - 1;
+    for(const auto &i: k){
+      out[count] = i;
+      cout << i << " " << out << endl;
+      count--;
+    }
+    cout << endl;
+    cout << out << endl;
+    char gout = out.to_ullong();
+    cout << gout;*/
 }
--- VL03-20240121/betriebsmodi.h
+++ VL03-20240121/betriebsmodi.h
@@ -2,39 +2,81 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <bitset>
 using namespace std;
 
-
-void blackbox(vector<char> input, ofstream& k_text){
-  for(const char &i : input){
-    k_text << i;
+void blackbox(vector<bool> block, ofstream& output){
+  bitset<8> out;
+  size_t count = out.size()-1;
+  for(const auto &i : block){
+    if(count > 0){
+      out[count] = i;
+      count --;
+    }else{
+      out[count] = i;
+      count= out.size()-1;    
+      char text_out = out.to_ullong();
+      output << text_out;
+    }
   }
 }
 
-void ECB (const size_t t, ifstream& text, ofstream& k_text){ 
-  const size_t block_size = t / 8; //da man mit char arbeitet und jeweils eins davon 8 Bit groß ist 
-  int text_size;
+void char_to_bin (vector<char> block , vector<bool>& k){
+
+    bitset<8> one_char;
+    for(const char &c: block){
+      one_char = c;
+      for(int i = one_char.size() - 1; i >= 0; i--){
+        k.push_back(one_char[i]);
+      }
+    }
+
+    
+}
+
+void print_block(vector<bool> block){
+  
+  for(const auto &i : block){
+    cout << i;
+  }
+  cout << endl;
+}
+
+void fill_zero(vector<bool>& block, size_t t){
+  size_t size = block.size();
+  cout << size << endl;
+  while(size != t){
+    block.push_back(0);
+    size++;
+  }
+  print_block(block);
+}
+
+void ECB (size_t t, ifstream& input, ofstream& output){
+  vector<bool> block(t);
+  vector<char> char_block(t/8);
+  block.clear();
+  char_block.clear();
   char byte = 0;
-
-  vector <char > block; 
-  size_t block_count = 0;
-
-  while (text.get(byte))
+  size_t count = 0;
+  size_t max_char = t/8;
+  while(input.get(byte))
   {
-    if(block_count != block_size){
-      block.push_back(byte);
-      block_count++;
-    }else{
-      blackbox(block,k_text);
+    if(count != max_char){
+      char_block.push_back(byte);
+      count++;
+    }
+    if(count == max_char){
+      char_to_bin(char_block,block);
+      blackbox(block, output);
       block.clear();
-      block_count = 1;
-      block.push_back(byte);
+      char_block.clear();
+      count=0;
     }
   }
-  if(block_count != block_size){
-    for(int i = block_count; i < block_size; i++){
-      block.push_back('0');
-    }
-    blackbox(block,k_text);
+  if(count != max_char){
+    char_to_bin(char_block,block);
+    fill_zero(block, t);
+    blackbox(block, output);
   }
 }
